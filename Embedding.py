7e4107749a41babf8d9836cebed2ea3e33c01ee8
JustGotTo <vovavickers@gmail.com>
2026-05-10 11:53:50 +0100

    Updated the logic of BytePairEncoder, so that now it creates multiple merges. Created file Embedding.py

--- Embedding.py
+++ Embedding.py
@@ -1,6 +1,14 @@
 from BytePairEncoder import BytePairEncoder
 
-import torch as t
-import torch.nn.functional as F
-import numpy as np
-import pandas as pd
+import torch.nn as nn
+
+class Embedding(nn.Module):
+    def __init__(self, prompt, vocab_size=1000, embedding_dim=64, hidden_size=384, output_size=384):
+        super().__init__()
+        self.prompt = BytePairEncoder(prompt=prompt).forward(prompt)
+        self.embedding_dim = embedding_dim #same as input_dim
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
+
+    def forward(self, input):
+        return self.embedding(input) #Simple embedding mechanism
