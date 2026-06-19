347432b0c909fc4a55d28e871ed5e138d15c4203
JustGotTo <vovavickers@gmail.com>
2026-06-19 13:36:03 +0100

    Minor updates

--- Backend.py
+++ Backend.py
@@ -92,16 +92,17 @@ class WordFeedForward(nn.Module):
         return x
 
 class AdaptiveMultiheadMaskedAttention(nn.Module):
-    def __init__(self, batch_size:int, full_size, mask_window_size, num_heads, embedding_size=386, eps=1e-8, prompt = None): #full_size - size of prompt before splitting the tokens into batches
+    def __init__(self, batch_size:int, full_size, mask_window_size, embedding_size=512, eps=1e-8, prompt = None): #full_size - size of prompt before splitting the tokens into batches
         super().__init__()
         self.eps = eps
         self.mask_window_size = mask_window_size if mask_window_size < batch_size else batch_size//2
         self.num_heads = t.floor((mask_window_size + full_size)/(embedding_size + 1))
         self.batch_size = batch_size
         self.t_beliefs = BeliefsLayer(full_size, embedding_size, window_size=self.mask_window_size)
-        self.t_context = t.tensor(np.random.rand(batch_size, embedding_size)) #tensor for context
+        self.t_context = AddNorm(self.embedding_size) #tensor for context
         self.mask = self.createMask() #creates a mask of the batch_size x batch_size matrix
         self.prompt = prompt
+        self.dph = self.embedding_size//self.num_heads
 
     def createMask(self):
         mask = t.ones(self.batch_size, self.batch_size)
@@ -116,10 +117,24 @@ class AdaptiveMultiheadMaskedAttention(nn.Module):
         return mask
 
     def split_batch(self, x, prompt):
-        mean_sentence_length = self.getMeanSentenceLength(prompt)
+        chunk_size = 256
+        sliding_window = 64
+        self.batch_size = chunk_size+sliding_window
+        chunks = t.empty(prompt//chunk_size)
+        i=0
+        while i < prompt.length:
+            if i == 0:
+                chunks[0] = x[0:chunk_size]
+            else:
+                try:
+                    chunks[i] = x[i+(chunk_size*i)-sliding_window:i+(chunk_size*i)+sliding_window]
+                except:
+                    chunks[i] = x[i+(chunk_size*i)-sliding_window:]
+            i += 1
 
-        x = x[..., 0::mean_sentence_length] #dividing the tensor into chunks of mean_sentence_length tokens
-        return x
+        return chunks
+
+
 
     def getMeanSentenceLength(self, prompt):
         sentences = [s.strip() for s in re.findall(r'[^.!?]*\.', prompt)]
@@ -127,13 +142,38 @@ class AdaptiveMultiheadMaskedAttention(nn.Module):
 
 
     def forward(self, x):
-        x = self.split_batch(x, self.prompt)
+        x = self.split_batch(x, self.prompt) #Splitting prompt into chunks
+        num_heads = self.num_heads
+        dph = self.dph #dims per head
+        w_q = t.tensor((self.embedding_size, dph))
+        w_k = t.tensor((self.embedding_size, dph))
+        w_v = t.tensor((self.embedding_size, dph))
+        w_b = t.tensor((self.embedding_size, dph))
+        result = t.zeros((self.batch_size, self.embedding_size))
+
+        for chunk in x:
+            chunk_beliefs = self.t_beliefs(chunk) #beliefs in the chunk
+            Q = t.matmul(chunk, w_q)
+            K = t.matmul(chunk, w_k)
+            V = t.matmul(chunk, w_v)
+            B = t.matmul(chunk_beliefs, w_b)
+            t_context = self.t_context()
+            chunk_context = t.matmul(self.t_context, w_b)
+            chunk_context = chunk_context.repeat(num_heads, 1, 1)
+            chunk_context = chunk_context.view(self.batch_size, num_heads, dph)
+            chunk_context = chunk_context.permute(1, 0, 2)
+            chunk_context = chunk_context.repeat(self.batch_size, 1, 1)
+            chunk_context = chunk_context.view(self.batch_size, num_heads, dph)
+
+
+
+
 
 
 
 class BeliefsLayer(nn.Module):
     #Beliefs layer is a simple attention mechanism that is used to calculate beliefs of the agent.
-    def __init__(self, hidden_size, output_size, window_size=1, embedding_size=386, eps=1e-8):
+    def __init__(self, hidden_size, output_size, window_size=1, embedding_size=512, eps=1e-8):
         super().__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -159,7 +199,3 @@ class BeliefsLayer(nn.Module):
         a = 0.99
         beliefs = t.mul(a,x) - (1-a)*topk(x, self.window_size) #Significance of window_size is determined by the size of the prompt
         return beliefs
-
-class ContextLayer(nn.Module):
-    def __init__(self, hidden_size, output_size, window_size=1, embedding_size=386, eps=1e-8):
-        super().__init__()
